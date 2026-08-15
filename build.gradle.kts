ee97d170ec475ad8e4ae56f2463ae58eaba3f5ce
Armen Sarkisyan <shashluchok96@gmail.com>
2026-08-15 20:00:11 +0300

    Add ktlint format precommit invokation
    (cherry picked from commit b417fd3fafecab883ccb4c56dce02daa28ec3206)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.gradle.api.file.FileTree
 import org.gradle.api.provider.Provider
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
 import org.jlleitschuh.gradle.ktlint.tasks.KtLintCheckTask
+import org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask
 
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
@@ -80,6 +81,11 @@ subprojects {
             }
         }
         tasks.withType<KtLintCheckTask>().configureEach {
+            if (project.hasProperty("precommit")) {
+                scopeToGitStagedFiles(project) { setSource(it) }
+            }
+        }
+        tasks.withType<KtLintFormatTask>().configureEach {
             if (project.hasProperty("precommit")) {
                 scopeToGitStagedFiles(project) { setSource(it) }
             }
